630999ec93ac1907311599068667863e6f7c82d4
confett0 <118047961+confett0@users.noreply.github.com>
2024-07-04 18:11:15 +0200

    Add dark mode button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ export default function Header() {
     return (
         <header>
             <h1 className='logo'>Todo</h1>
+            <button className="dark-mode-button"><img src="./icon-moon.svg"/></button>
         </header>
     )
 }
